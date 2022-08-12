12b1cce7d953c2c67f5582b30f4e054d46d52f64
Mauricio Sevilla <jmsevillam@unal.edu.co>
2022-08-12 16:11:07 +0200

    Main code running

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "ising.h"
+#include <cstdlib>
+#include <cmath>
+int main(int argc, char const *argv[]) {
+  ising model;
+  model.Initial_Conditions(atoi(argv[1]),0.1);
+  int Num=atoi(argv[2]);
+  int iter=atoi(argv[3]);
+  double dT=(4.0)/(Num+1.0);
+  for (double T=0;T<4;T+=dT){
+    model.Temp(T);
+    for(int i=0;i<iter;i++){
+      model.Time_Step();
+    }
+    printf("%f %f %f\n",T,model.Magne(),model.Susce());
+  }
+  return 0;
+}
